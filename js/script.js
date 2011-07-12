f649a27907bccc9bb1faad6aee7a11cc333e477b
Majd Taby <mtaby@me.com>
2011-07-12 00:56:45 -0700

    display nested list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $(function(){
 
-});
+var num = 100,
+    depth = 4,
+    text = "";
+
+for (var i = depth - 1; i >= 0; i--){
+  text += "<ul>";
+  for (var j = num - 1; j >= 0; j--){
+    text += '<li> Item number '+j;
+  }
+}   
+for (var i = depth - 1; i >= 0; i--){
+  text += "</ul>";
+}
+
+$('#list').append(text);
+
+$(document).scroll(function(evt) {
+  var scroll = document.body.scrollTop;
+})
+
+});
